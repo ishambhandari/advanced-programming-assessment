3f2c4c2aa37814a16202dbba0f8c6bea170fb9bf
Isham Bhandari <ishambhandari007@gmail.com>
2023-03-03 12:14:48 +0000

    adding pagination for the number of records

--- server.py
+++ server.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import sqlite3
+import math
+
 app = Flask(__name__)
 
 con = sqlite3.connect('database.db', check_same_thread=False)
@@ -10,10 +12,19 @@ def home():
     return render_template('home.html')
 @app.route('/sales', methods=['GET'])
 def sales():
-    sales_data_initial = curs.execute('select * from sales')
+    page = request.args.get('page', default=1, type=int)
+    per_page = request.args.get('per_page', default=10, type=int)
+    offset = (page-1) * per_page
+    limit = per_page
+    sales_data_initial = curs.execute('select * from sales  LIMIT ? OFFSET ?', (limit,offset))
     sales_data = sales_data_initial.fetchall()
+     # Count total number of records for pagination
+    total_records = curs.execute('SELECT COUNT(*) FROM sales').fetchone()[0]
+
+    # Calculate total number of pages for pagination
+    total_pages = math.ceil(total_records / per_page)
     print(sales_data_initial.description)
-    return render_template('sales.html', data = sales_data)
+    return render_template('sales.html', data = sales_data, page = page, per_page = per_page, total_pages = total_pages)
 
 @app.route('/customers', methods=['GET'])
 def customers():
